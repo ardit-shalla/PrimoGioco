0830ae6493fc06a10806992f33421cf227b9f12c
Ardit Shalla <ardit.shalla@itcubeconsulting.com>
2022-03-09 10:08:51 +0100

    maven jar

--- src/main/java/it/itcubeconsulting/Giocatore2.java
+++ src/main/java/it/itcubeconsulting/Giocatore2.java
@@ -16,7 +16,7 @@ public class Giocatore2 extends Giocatore1{
         if (armatura){
             this.salute -=20;
             if (this.salute <=0) this.salute = 0;
-            System.out.println("Armatura attiva. Colpito da armi1. Salute e rodotto di 20." + ". Nuovi punti salute: " + this.salute);
+            System.out.println("Armatura attiva. Colpito da armi1. Salute e ridotto di 20." + ". Nuovi punti salute: " + this.salute);
         }if (!armatura){
             this.salute -=30;
             if (this.salute <=0) this.salute = 0;
